ee027c0116ad008cad9f7a70d7e1c402346da48c
emilykaufman <54636206+emilykaufman@users.noreply.github.com>
2020-06-30 10:32:17 -0500

    start

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Register User</title>
+</head>
+<body>
+    <form action="register.php" method="POST">
+        <input type="text" name="first" placeholder="Firstname"><br>
+        <input type="text" name="last" placeholder="Lastname"><br>
+        <input type="text" name="email" placeholder="Email address"><br>
+        <input type="text" name="username" placeholder="Username"><br>
+        <input type="password" name="password" placeholder="Password"><br>
+        <button type="submit" name="submit">Register!</button>
+    </form>
+</body>
